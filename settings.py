0dc6df727f8ffa67069957801d059bb54e9094dd
noahgill409 <noahgill409@gmail.com>
2022-01-03 17:54:53 -0600

    trim settings
    trim some personal settings, update .gitignore

--- settings.py
+++ settings.py
@@ -5,18 +5,14 @@ HUE_BRIDGE_IP = '192.168.1.100'
 HUE_BRIDGE_USERNAME = 'HLtg5osJJGjwVXKt2jmdZuC-UnEe1d1kUmrCPeLq'
 
 # directory settings
-AKIKO_DIR = pathlib.Path('album-covers-akiko')
-REIMU_DIR = pathlib.Path('reimus')
-GENERAL_AC_DIR = pathlib.Path('album-covers-general')
-ANIME_OPED_DIR = pathlib.Path('anime-oped')
-PALETTE_DIR = pathlib.Path('palettes')
-GAUSS_DIR = pathlib.Path('gauss')
-QUANT_DIR = pathlib.Path('quant')
-PINKS_DIR = pathlib.Path('pinks')
+OUTPUT_DIR = pathlib.Path('output')
+PALETTE_DIR = OUTPUT_DIR.joinpath('palettes')
+GAUSS_DIR = OUTPUT_DIR.joinpath('gauss')
+QUANT_DIR = OUTPUT_DIR.joinpath('quant')
 
 # Pillow settings
 QUANTIZED_DIMENSION = 128   # pixels
-FONT = 'DroidSans.ttf'
+# FONT = 'DroidSans.ttf'
 
 # pandas
 CSV_SEP = '\t'          # sep char to use for imported and exported csv's
