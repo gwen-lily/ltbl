62ab82a0f7954e1545a90ac1e55c1c50dbdf4b9c
noahgill409 <noahgill409@gmail.com>
2022-08-24 19:53:49 -0500

    preserve light state before and after tests

--- src/ltbl/utils.py
+++ src/ltbl/utils.py
@@ -5,6 +5,11 @@ from pathlib import Path
 
 import PIL
 from PIL import Image
+from phue import Light, Bridge
+
+from ltbl.data import LightsScheme
+
+from .network import BRIDGE
 
 
 def is_image_file(__file: PathLike, /) -> bool:
@@ -27,3 +32,24 @@ def is_image_file(__file: PathLike, /) -> bool:
             return True
     except PIL.UnidentifiedImageError:
         return False
+
+
+def get_lights_list(__bridge: Bridge = BRIDGE, /) -> list[Light]:
+    """Returns a type-validated list of Lights."""
+
+    lights = __bridge.get_light_objects(mode="list")
+    assert all(isinstance(li, Light) for li in lights)
+    return lights
+
+
+def get_lights_by_id(__bridge: Bridge = BRIDGE, /) -> dict[int, Light]:
+    """Returns a type-validated dictionary of lights."""
+
+    lights: dict[int, Light] = __bridge.get_light_objects(mode=LightsScheme.ID)
+
+    try:
+        assert isinstance(lights, dict)
+    except AssertionError:
+        print(lights, type(lights))
+
+    return lights
--- tests/ltbl/test_routine.py
+++ tests/ltbl/test_routine.py
@@ -1,9 +1,15 @@
 """Test the routine sub-module."""
 
+from dataclasses import dataclass, field
+from typing import Any
 from unittest import TestCase, main
+
+from phue import Light, Bridge
 
 from ltbl import BRIDGE, Color
 from ltbl.routine import LoopLights, RandomWalk
+from ltbl.routine.data import LightState
+from ltbl.utils import get_lights_by_id
 
 Red = Color(255, 0, 0)
 Green = Color(0, 255, 0)
@@ -11,10 +17,23 @@ Blue = Color(0, 0, 255)
 
 RGB_Palette = [Red, Green, Blue]
 
-_TEST_TIME_LIMIT = 15  # seconds
+_TEST_TIME_LIMIT = 10  # seconds
+
+LIGHT_ID_DICT: dict[int, LightState] = {}
 
 
 class TestRoutine(TestCase):
+    """Test the routine sub-module."""
+
+    def test_record_lights(self):
+        """Test the test's ability to save the light state"""
+        lights = BRIDGE.lights_by_id
+
+        for light_id, light in lights.items():
+            assert isinstance(light_id, int) and isinstance(light, Light)
+            state = LightState(Color(*light.xy), light.brightness)
+            LIGHT_ID_DICT[light_id] = state
+
     def test_LoopLights(self):
         routine = LoopLights(BRIDGE.lights, RGB_Palette, time_limit=_TEST_TIME_LIMIT)
         routine.loop()
@@ -23,6 +42,15 @@ class TestRoutine(TestCase):
         routine = RandomWalk(BRIDGE.lights, RGB_Palette, time_limit=_TEST_TIME_LIMIT)
         routine.loop()
 
+    def test_reset_lights(self):
+        """Test the test's ability to reset the lights"""
+        for light_id, (color, brightness) in LIGHT_ID_DICT.items():
+            light = BRIDGE[light_id]
+            assert isinstance(light, Light)
+
+            light.xy = color.xy
+            light.brightness = brightness
+
 
 if __name__ == "__main__":
-    main()
+    main()  # run tests
